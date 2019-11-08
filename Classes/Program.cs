beda91619bdf960d5fc7eb540b3a37a3ad84162f
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-08 21:59:14 +0200

    Constructors

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -13,10 +13,19 @@ namespace Classes
             //person.Name = "Ted";
 
             // 2nd way of creating an instance of a class (with static)
-            var person = Person.Parse("Ted");
-            person.Introduce("Mike");
+            //var person = Person.Parse("Ted");
+            //person.Introduce("Mike");
             //-------------------------------------------------------------
-            
+
+            // Lesson 2 : Constructors
+
+            var customer = new Customer(1,"Ted");
+
+            var order = new Order();
+            customer.Orders.Add(order);
+
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
         }
     }
 }

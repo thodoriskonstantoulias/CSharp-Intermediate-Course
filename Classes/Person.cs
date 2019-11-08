ef12054186dadebd516566ab40ca0bf30d5c6c2e
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-08 21:31:33 +0200

    Introduction to classes and instances

---
+++ Classes/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string name)
+        {
+            Console.WriteLine("Hello {0}, I am {1} ", name, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+            return person;
+        }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lesson 1 : Introduction to classes
+
+            // 1st way of creating an instance of a class
+            //var person = new Person();
+            //person.Name = "Ted";
+
+            // 2nd way of creating an instance of a class (with static)
+            var person = Person.Parse("Ted");
+            person.Introduce("Mike");
+            //-------------------------------------------------------------
+            
+        }
+    }
+}

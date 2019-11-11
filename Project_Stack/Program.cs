f0261be239fec8346921c574a2e7297e8250a0ad
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-11 21:14:28 +0200

    Project Stack

---
+++ Project_Stack/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Project_Stack
+{
+    //A Stack is a data structure for storing a list of elements in a LIFO(last in, first out) fashion.
+    //Design a class called Stack with three methods.
+    //void Push(object obj)
+    //object Pop()
+    //void Clear()
+    //The Push() method stores the given object on top of the stack. We use the “object” type here so
+    //we can store any objects inside the stack.Remember the “object” class is the base of all classes
+    //in the.NET Framework.So any types can be automatically upcast to the object. Make sure to
+    //take into account the scenario that null is passed to this object. We should not store null
+    //references in the stack. So if null is passed to this method, you should throw an
+    //InvalidOperationException.
+    //The Pop() method removes the object on top of the stack and returns it.Make sure to take into
+    //account the scenario that we call the Pop() method on an empty stack. In this case, this method
+    //should throw an InvalidOperationException
+    //The Clear() method removes all objects from the stack.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ Project_Stack/Stack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_Stack
+{
+    public class Stack
+    {
+        private readonly List<object> _list = new List<object>();
+        public void Push(object obj)
+        {
+            if (obj == null)
+            {
+                throw new InvalidOperationException("You must pass a non nullable object");
+            }
+            _list.Add(obj);
+
+        }
+        public object Pop()
+        {
+            if (_list.Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            var last = _list[_list.Count - 1];
+            _list.RemoveAt(_list.Count - 1);
+            return last;
+        }   
+        public void Clear()
+        {
+            _list.Clear();
+        }
+    }
+}
